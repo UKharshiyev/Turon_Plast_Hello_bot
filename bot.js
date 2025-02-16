b8d7b6642c4be2136545aaa81a3e44242932a1cd
Qarshiyev <ulugbek.kharshiyev@mail.ru>
2025-02-16 21:55:15 +1200

    First commit

---
+++ bot.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = '7863973371:AAGhTYXyDY9DWz6sVij3r-XMYjGXS3wxZ8U';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", msg => {
+  console.log(msg); // Guruhdagi barcha xabarlarni terminalda ko'rish
+
+    if (msg.new_chat_members) {
+        msg.new_chat_members.forEach(user => {
+            const chatId = msg.chat.id;
+            const username = user.username
+                ? `@${user.username}`
+                : user.first_name;
+
+            bot.sendMessage(
+                chatId,
+                `Assalomu alaykum ${username}
+❇️ Turon Sam Plast MCHJ endi telegramda. 
+
+📖 Turon Sam Plast MCHJ:
+✅ Plyonkalarimiz eniga 3.m dan 16.m gacha
+✅ Polietilen trubalar
+✅ Layflat kapelniy sistemalar
+
+♻️ Ishlab chiqarish va yetkazib berish xizmati
+
+🌐 Bizning manzil: Samarqand viloyati , Kattaqurg'on tumani Mullako'rpa MFY
+
+🚚 Yetkazib berish 100.km radiusgacha ✅ BEPUL.
+
+1⃣ Buyurtma berish uchun bizning 🤖Bot manzilimizga muroojat qiling:    
+👉 @turonplastclientbot
+
+⚠️ yoki
+
+2⃣ Muroojat uchun telefonlar: 
+
+☎️  +998332253333
+☎️ +998942253333
+☎️ +998948343333
+☎️ +998948353333
+
+❗️Muroojat qilish vaqti: 08:00 dan 20:00 gacha.`
+            );
+        });
+    }
+});
